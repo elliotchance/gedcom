f430b41d3d22cc68c3d3dbf52818dd85787e90da
Elliot Chance <elliotchance@gmail.com>
2018-08-03 00:20:12 +1000

    Added StringSimilarity()

--- jaro.go
+++ jaro.go
@@ -2,6 +2,8 @@ package gedcom
 
 import (
 	"math"
+	"regexp"
+	"strings"
 )
 
 // JaroWinkler distance. JaroWinkler returns a number between 0 and 1 where 1
@@ -9,6 +11,12 @@ import (
 // on Record Linkage stuff, thus it tries to be accurate for real names and
 // common typos. You should consider it on data such as person names and street
 // names.
+//
+// JaroWinkler is sensitive to punctuation and capitalization of strings. If you
+// are comparing names or places you may want to use StringSimilarity instead.
+// StringSimilarity removes punctuation, extra whitespace and normalises
+// capitalization before performing the comparison which almost always delivers
+// more desirable results.
 //
 // JaroWinkler is a more accurate version of the Jaro algorithm. It works by
 // boosting the score of exact matches at the beginning of the strings. By doing
@@ -83,4 +91,16 @@ func jaro(a, b string) float64 {
 	transposes := math.Floor(float64(halfs / 2))
 
 	return ((matches / la) + (matches / lb) + (matches-transposes)/matches) / 3.0
+}
+
+// StringSimilarity is a less sensitive version of a JaroWinkler string
+// comparison. It is the ideal choice to compare strings that represent
+// individual names, places, etc as it does not take into account
+// capitalization, punctuation and extra spaces.
+func StringSimilarity(a, b string) float64 {
+	re := regexp.MustCompile("[^a-z0-9 ]")
+	a = re.ReplaceAllString(strings.ToLower(a), "")
+	b = re.ReplaceAllString(strings.ToLower(b), "")
+
+	return JaroWinkler(CleanSpace(a), CleanSpace(b))
 }
--- jaro_test.go
+++ jaro_test.go
@@ -1,51 +1,63 @@
 package gedcom_test
 
 import (
+	"github.com/elliotchance/gedcom"
 	"github.com/stretchr/testify/assert"
 	"testing"
-	"github.com/elliotchance/gedcom"
 )
 
 var stringTests = []struct {
-	a, b string
-	want float64
+	a, b      string
+	jaro, str float64
 }{
 	// Simple values
-	{"", "", 0},
-	{"Foo", "Foo", 1},
-	{"foo bar", "foo bar", 1},
-	{"Foo bar", "foo bar", 0.9333333333333333},
-	{"foo bar", "Foo Bar", 0.8666666666666668},
-	{"foo", "bar", 0},
+	{"", "", 0, 0},
+	{"Foo", "Foo", 1, 1},
+	{"foo bar", "foo bar", 1, 1},
+	{"Foo bar", "foo bar", 0.9333333333333333, 1},
+	{"foo bar", "Foo Bar", 0.8666666666666668, 1},
+	{"foo", "bar", 0, 0},
 
 	// Names
-	{"Elliot Chance", "Elliot R. Chance", 0.9163461538461538},
-	{"Elliot R Chance", "Elliot Chance", 0.9271794871794872},
-	{"Elliot Rupert Chance", "Elliot R. Chance", 0.8975},
-	{"Elliot Rupert Chance", "Elliot R d P Chance", 0.8678947368421053},
-	{"Eliot Rupert Chance", "Elliot R. Chance", 0.7822055137844612},
-	{"J Smith", "John Smith", 0.7814285714285714},
-	{"John Smeeth", "John Smith", 0.9214141414141415},
-	{"bob jones", "Bob Jones", 0.8962962962962963},
+	{"Elliot Chance", "Elliot R. Chance", 0.9163461538461538, 0.9271794871794872},
+	{"Elliot R Chance", "Elliot Chance", 0.9271794871794872, 0.9271794871794872},
+	{"Elliot Rupert Chance", "Elliot R. Chance", 0.8975, 0.91},
+	{"Elliot Rupert Chance", "Elliot R d P Chance", 0.8678947368421053, 0.8784210526315789},
+	{"Eliot Rupert Chance", "Elliot R. Chance", 0.7822055137844612, 0.7977610693400166},
+	{"J Smith", "John Smith", 0.7814285714285714, 0.7814285714285714},
+	{"John Smeeth", "John Smith", 0.9214141414141415, 0.9214141414141415},
+	{"bob jones", "Bob Jones", 0.8962962962962963, 1},
+	{"Elliot   Chance", "Elliot Chance", 0.9271794871794872, 1},
+	{"Elliot Rupert Chance", "Elliot 'Rupert' Chance", 0.9218181818181819, 1},
 
 	// Places
-	{", Connecticut", "Connecticut", 0.7972027972027972},
+	{", Connecticut", "Connecticut", 0.7972027972027972, 1},
 	{
 		"28 Leinster Gardens, London, England, United Kingdom",
 		"40 Augustus Rd, Birmingham, England, United Kingdom",
 		0.6681987417281535,
+		0.6800831443688585,
 	},
 	{
 		"Adams County, Adams County, Iowa, United States",
 		"Adams, Umatilla County, Oregon, United States",
 		0.8408101718740018,
+		0.8632912132912133,
 	},
 }
 
 func TestJaroWinkler(t *testing.T) {
 	for _, test := range stringTests {
 		t.Run(test.a+"_"+test.b, func(t *testing.T) {
-			assert.Equal(t, test.want, gedcom.JaroWinkler(test.a, test.b))
+			assert.Equal(t, test.jaro, gedcom.JaroWinkler(test.a, test.b))
+		})
+	}
+}
+
+func TestStringSimilarity(t *testing.T) {
+	for _, test := range stringTests {
+		t.Run(test.a+"_"+test.b, func(t *testing.T) {
+			assert.Equal(t, test.str, gedcom.StringSimilarity(test.a, test.b))
 		})
 	}
 }
