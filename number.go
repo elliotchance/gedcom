39ef836204ada3ac9ba718a4f5562fca12fee7dc
Elliot Chance <elliotchance@gmail.com>
2018-10-31 12:02:35 +1100

    Add Number type
    Number can be used to transform a number into another representation such as roman numerals.
    
    It is not used anywhere in the library yet, but it will be when we start to introduce relationships.

---
+++ number.go
@@ -0,0 +1,126 @@
+package gedcom
+
+import (
+	"strings"
+	"fmt"
+)
+
+// Number can be used to transform a number into another representation.
+//
+// Even though a Number is an alias for int you should use the provided function
+// Int() to get its value in case the underlying type changes in the future.
+//
+// It should also be noted that Number only supports integer values and many of
+// the conversions or representations will only work with positive values. Some
+// functions may also have more specific ranges.
+type Number int
+
+// NewNumberWithInt is the preferred way to create a new Number.
+//
+// While it is possible to cast the value directly to a Number, using this
+// constructor is safer if the underlying type of Number changes.
+func NewNumberWithInt(n int) Number {
+	return Number(n)
+}
+
+// Int returns the true value of the number with no loss of precision.
+func (n Number) Int() int {
+	return int(n)
+}
+
+// UpperRoman returns uppercase Roman numerals, like "VII".
+//
+// The acceptable range is between 0 and 10000. Zero is a special case that will
+// return "N", an abbreviation for "nulla". While it is possible to represent
+// numbers number larger than 10000 it would get very cumbersome since each
+// thousand would need an extra "M".
+//
+// If the number is outside of the acceptable range then an empty string is
+// returned with an error.
+//
+// Also see LowerRoman.
+func (n Number) UpperRoman() (string, error) {
+	// Catch edge cases first.
+	switch {
+	case n == 0:
+		return "N", nil
+
+	case n < 0:
+		return "", fmt.Errorf("negative number: %d", n)
+
+	case n > 9999:
+		return "", fmt.Errorf("number is greater than 9999: %d", n)
+	}
+
+	// Some of the following logic was copied from:
+	// https://github.com/StefanSchroeder/Golang-Roman/blob/master/roman.go
+	figure := []int{1000, 100, 10, 1}
+
+	romanDigitA := []string{
+		1:    "I",
+		10:   "X",
+		100:  "C",
+		1000: "M",
+	}
+
+	romanDigitB := []string{
+		1:    "V",
+		10:   "L",
+		100:  "D",
+		1000: "M",
+	}
+
+	arg := n.Int()
+	ret := ""
+	var romanSlice []string
+	x := ""
+
+	// Correct for values greater than 4000.
+	if arg >= 4000 {
+		ms := arg / 1000
+		ret = strings.Repeat("M", ms)
+		arg -= ms * 1000
+	}
+
+	for _, f := range figure {
+		digit, i, v := int(arg/f), romanDigitA[f], romanDigitB[f]
+		switch digit {
+		case 1:
+			romanSlice = append(romanSlice, string(i))
+		case 2:
+			romanSlice = append(romanSlice, string(i)+string(i))
+		case 3:
+			romanSlice = append(romanSlice, string(i)+string(i)+string(i))
+		case 4:
+			romanSlice = append(romanSlice, string(i)+string(v))
+		case 5:
+			romanSlice = append(romanSlice, string(v))
+		case 6:
+			romanSlice = append(romanSlice, string(v)+string(i))
+		case 7:
+			romanSlice = append(romanSlice, string(v)+string(i)+string(i))
+		case 8:
+			romanSlice = append(romanSlice, string(v)+string(i)+string(i)+string(i))
+		case 9:
+			romanSlice = append(romanSlice, string(i)+string(x))
+		}
+
+		arg -= digit * f
+		x = i
+	}
+
+	for _, e := range romanSlice {
+		ret += e
+	}
+
+	return ret, nil
+}
+
+// LowerRoman returns the lowercase Roman numerals, like "vii".
+//
+// LowerRoman works with exactly the same rules and constraints as UpperRoman.
+func (n Number) LowerRoman() (string, error) {
+	roman, err := n.UpperRoman()
+
+	return strings.ToLower(roman), err
+}
---
+++ number_test.go
@@ -0,0 +1,124 @@
+package gedcom_test
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/elliotchance/tf"
+	"github.com/elliotchance/gedcom"
+)
+
+var numberTests = map[gedcom.Number]struct {
+	upperRoman string
+	lowerRoman string
+	err        error
+}{
+	// 0 is a special case.
+	0: {"N", "n", nil},
+
+	// We check all of the numbers up to 20 as that will cover all the
+	// combinations with several different roman numerals.
+	1:  {"I", "i", nil},
+	2:  {"II", "ii", nil},
+	3:  {"III", "iii", nil},
+	4:  {"IV", "iv", nil},
+	5:  {"V", "v", nil},
+	6:  {"VI", "vi", nil},
+	7:  {"VII", "vii", nil},
+	8:  {"VIII", "viii", nil},
+	9:  {"IX", "ix", nil},
+	10: {"X", "x", nil},
+	11: {"XI", "xi", nil},
+	12: {"XII", "xii", nil},
+	13: {"XIII", "xiii", nil},
+	14: {"XIV", "xiv", nil},
+	15: {"XV", "xv", nil},
+	16: {"XVI", "xvi", nil},
+	17: {"XVII", "xvii", nil},
+	18: {"XVIII", "xviii", nil},
+	19: {"XIX", "xix", nil},
+	20: {"XX", "xx", nil},
+
+	// These are just some random prime numbers we see up to 1000.
+	29:  {"XXIX", "xxix", nil},
+	71:  {"LXXI", "lxxi", nil},
+	113: {"CXIII", "cxiii", nil},
+	229: {"CCXXIX", "ccxxix", nil},
+	349: {"CCCXLIX", "cccxlix", nil},
+	409: {"CDIX", "cdix", nil},
+	541: {"DXLI", "dxli", nil},
+	601: {"DCI", "dci", nil},
+	733: {"DCCXXXIII", "dccxxxiii", nil},
+	863: {"DCCCLXIII", "dccclxiii", nil},
+	941: {"CMXLI", "cmxli", nil},
+
+	// Larger numbers.
+	1000:  {"M", "m", nil},
+	1234:  {"MCCXXXIV", "mccxxxiv", nil},
+	2000:  {"MM", "mm", nil},
+	2001:  {"MMI", "mmi", nil},
+	2235:  {"MMCCXXXV", "mmccxxxv", nil},
+	3000:  {"MMM", "mmm", nil},
+	3001:  {"MMMI", "mmmi", nil},
+	4000:  {"MMMM", "mmmm", nil},
+	4001:  {"MMMMI", "mmmmi", nil},
+	5000:  {"MMMMM", "mmmmm", nil},
+	5001:  {"MMMMMI", "mmmmmi", nil},
+	9999:  {"MMMMMMMMMCMXCIX", "mmmmmmmmmcmxcix", nil},
+	10000: {"", "", errors.New("number is greater than 9999: 10000")},
+	10078: {"", "", errors.New("number is greater than 9999: 10078")},
+
+	// Invalid numbers.
+	-1:  {"", "", errors.New("negative number: -1")},
+	-45: {"", "", errors.New("negative number: -45")},
+}
+
+func TestNumber_UpperRoman(t *testing.T) {
+	for number, tt := range numberTests {
+		t.Run(fmt.Sprintf("%d", number), func(t *testing.T) {
+			got, err := number.UpperRoman()
+
+			assertError(t, tt.err, err)
+			assert.Equal(t, tt.upperRoman, got)
+		})
+	}
+}
+
+func TestNumber_LowerRoman(t *testing.T) {
+	for number, tt := range numberTests {
+		t.Run(fmt.Sprintf("%d", number), func(t *testing.T) {
+			got, err := number.LowerRoman()
+
+			assertError(t, tt.err, err)
+			assert.Equal(t, tt.lowerRoman, got)
+		})
+	}
+}
+
+func assertError(t *testing.T, expected, actual error) {
+	if expected == nil {
+		assert.NoError(t, actual)
+	} else {
+		assert.EqualError(t, expected, actual.Error())
+	}
+}
+
+func TestNumber_Int(t *testing.T) {
+	for number := range numberTests {
+		t.Run(fmt.Sprintf("%d", number), func(t *testing.T) {
+			got := number.Int()
+			assert.Equal(t, int(number), got)
+		})
+	}
+}
+
+func TestNewNumberWithInt(t *testing.T) {
+	NewNumberWithInt := tf.Function(t, gedcom.NewNumberWithInt)
+
+	NewNumberWithInt(0).Returns(gedcom.Number(0))
+	NewNumberWithInt(123).Returns(gedcom.Number(123))
+	NewNumberWithInt(-23).Returns(gedcom.Number(-23))
+	NewNumberWithInt(123456789).Returns(gedcom.Number(123456789))
+}
