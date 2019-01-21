889047366d29c8bb41498abc972d406b6e6d0a92
Elliot Chance <elliotchance@gmail.com>
2019-01-21 13:04:45 +1100

    Add MergeDocumentsAndIndividuals to gedcomq (#191)
    MergeDocumentsAndIndividuals(doc1, doc2) merges two documents while also merging similar individuals.

---
+++ q/merge_documents_and_individuals_expr.go
@@ -0,0 +1,39 @@
+package q
+
+import (
+	"errors"
+	"github.com/elliotchance/gedcom"
+	"fmt"
+)
+
+// MergeDocumentsAndIndividualsExpr is a function. See Evaluate.
+type MergeDocumentsAndIndividualsExpr struct{}
+
+// Evaluate merges two documents while also merging similar individuals.
+func (e *MergeDocumentsAndIndividualsExpr) Evaluate(engine *Engine, input interface{}, args []*Statement) (interface{}, error) {
+	if len(args) != 2 {
+		return nil, errors.New("MergeDocumentsAndIndividuals must take two arguments")
+	}
+
+	documents := []*gedcom.Document{}
+
+	for argNumber, arg := range args {
+		value, err := arg.Evaluate(engine, nil)
+		if err != nil {
+			return nil, err
+		}
+
+		if doc, ok := value.(*gedcom.Document); ok {
+			documents = append(documents, doc)
+		} else {
+			return nil, fmt.Errorf(
+				"argument %d of MergeDocumentsAndIndividuals is not a Document",
+				argNumber+1)
+		}
+	}
+
+	mergeFn := gedcom.EqualityMergeFunction
+	options := gedcom.NewIndividualNodesCompareOptions()
+
+	return gedcom.MergeDocumentsAndIndividuals(documents[0], documents[1], mergeFn, options)
+}
