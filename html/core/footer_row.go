0b7b052240e1f6c589a83a296651cdc2c2911703
Arul Selvan <aselvan@selvans.net>
2023-08-14 17:40:42 -0500

    Added timestamp to footer row

--- html/core/footer_row.go
+++ html/core/footer_row.go
@@ -1,6 +1,7 @@
 package core
 
 import "io"
+import "time"
 
 // FooterRow appears on all pages at the bottom.
 type FooterRow struct{}
@@ -15,9 +16,13 @@ func (c *FooterRow) WriteHTMLTo(w io.Writer) (int64, error) {
 		"https://github.com/elliotchance/gedcom",
 	)
 
+  curTime := time.Now()
+  layout := "Jan 2, 2006, 3.04 pm"
 	content := NewComponents(
 		NewText("Generated with "),
 		link,
+    NewText(" on "),
+    NewText(curTime.Format(layout)),
 	)
 
 	return NewComponents(
